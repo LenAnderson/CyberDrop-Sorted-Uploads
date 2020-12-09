be76017b6bb142b79a685035998d004a57abe883
Len <LenAnderson@users.noreply.github.com>
2020-12-09 09:27:15 +0100

    upload progress in tab title

--- CyberDrop_sorted_uploads.user.js
+++ CyberDrop_sorted_uploads.user.js
@@ -2,7 +2,7 @@
 // @name         CyberDrop - Sorted Uploads
 // @namespace    https://github.com/LenAnderson/
 // @downloadURL  https://github.com/LenAnderson/CyberDrop-Sorted-Uploads/raw/master/CyberDrop_sorted_uploads.user.js
-// @version      1.0.0
+// @version      1.1.0
 // @description  Sort uploads on CyberDrop.me by name
 // @author       LenAnderson
 // @match        https://cyberdrop.me/
@@ -21,16 +21,30 @@
 			dz = Dropzone.instances[0];
 		}
 		console.log('found dropzone');
+		const title = document.title;
+		let album = '';
+		let numFiles = 0;
+		let numDone = 0;
 		dz.options.autoProcessQueue = false;
 		dz.on('addedfiles', files=>{
 			console.log('addedfiles: ', files);
+			let albumOpt = document.querySelector('#albumSelect').selectedOptions[0];
+			album = albumOpt.value ? albumOpt.textContent : 'Uploading';
+			numFiles = files.length;
+			numDone = 0;
+			document.title = `${album} (${numDone}/${numFiles} | ${title}`;
 			dz.files.sort((a,b)=>a.name<b.name?1:a.name>b.name?-1:0);
 			dz.options.autoProcessQueue = true;
 			dz.processQueue();
 		});
+		dz.on('complete', ()=>{
+			numDone++;
+			document.title = `${album} (${numDone}/${numFiles} | ${title}`;
+		});
 		dz.on('queuecomplete', ()=>{
 			console.log('queuecomplete');
-			dz.autoProcessQueue = false;
+			dz.options.autoProcessQueue = false;
+			document.title = `Completed ${album} | ${title}`;
 		});
 	};
 	init();
